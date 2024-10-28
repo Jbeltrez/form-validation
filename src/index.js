db63778bbe2545577558d815bdc86cac960da80e
john beltrez <beltrezjohn1@gmail.com>
2024-10-28 11:21:29 -0500

    made sure error display box only shows upon completion if the username is left blank

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-let errorList = document.querySelector(".error-list");
+const errorList = document.querySelector(".error-list")
+const errorDisplay = document.querySelector("#errorDisplay")
+const registrationForm = document.querySelector("#registration"); 
+console.log(`registrationForm ----> ${registrationForm}`);
 
-function validateForm() {
-    let errors = document.querySelector("#errorDisplay")
-    errors.style.display = "flex";
-    let x = document.forms[0]["username"].value; 
-    if (x == "") {
-        // alert("username cannot be blank");
-        let e = document.createElement("li"); 
-        e.innerHTML = "Username cannot be blank";
-        errorList.appendChild(e); 
-            }
-            else {
-                console.log('hellno')
-            }
+registrationForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log("hello")
+    errorDisplay.style.display = "flex"; 
+    if (registrationForm["username"].value == "") {
+        let input = prompt("what do you think your error is ?")
+        let errorItem = document.createElement("li");
+        errorItem.innerHTML = input
+        errorList.appendChild(errorItem);
+    }
+})
+function validateForm(e) {
+    e.preventDefault();
+    console.log("hello")
+    // e.preventDefault();
+    // let errors = document.querySelector("#errorDisplay")
+    // // errors.style.display = "flex";
+    // let x = document.forms[0]["username"].value; 
+    // if (x == "") {
+    //     // alert("username cannot be blank");
+
+    //     errors.appendChild()
+    //     let e = document.createElement("li"); 
+    //     e.innerHTML = "Username cannot be blank";
+    //     errorList.innerText = "<p>There are some errors</p>"
+    //         }
+    //         else {
+    //             console.log('hellno')
+    //         }
 }
 
